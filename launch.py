dfedc995aa01df18b7e6eaea86b052112970e8c1
shawnfan19 <shawnclarkefan@gmail.com>
2025-05-20 17:09:50 +0200

    restructure launch.py to parallelize overrides

--- launch.py
+++ launch.py
@@ -1,17 +1,34 @@
 import os
+from enum import Enum
 import subprocess
 import sys
 from dataclasses import dataclass, field
 from typing import Optional
-
 from omegaconf import OmegaConf
+
+
+class CPUQueue(Enum):
+    SHORT = "short"
+    MEDIUM = "medium"
+    LONG = "long"
+    VERY_LONG = "very-long"
+    HIGH_MEM = "high-mem"
+    INTERACTIVE = "interactive"
+    INTERACTIVE_LONG = "interactive-long"
+
+
+class GPUQueue(Enum):
+    DEBIAN = "gpu-debian"
+    LOWPRIO_DEBIAN = "gpu-lowprio-debian"
+    INTERACTIVE = "gpu-default-interactive"
 
 
 @dataclass
 class RunConfig:
-    name: str = "debug"
+    submit_script: str = "./submit/dkfz/submit.sh"
     script: str = "apps/train.py"
     script_args: str = ""
+    script_overrides: list = field(default_factory=list)
     memory: int = 16
     gpu: bool = True
     gpu_num: int = 1
@@ -24,13 +41,13 @@ class RunConfig:
     whitelist: list = field(default_factory=list)
 
 
-@dataclass
-class ExperimentConfig:
-    submit_script: str = "./submit/dkfz/submit.sh"
-    runs: list[RunConfig] = field(default_factory=list)
-
 
 def validate_run_config(run_config: RunConfig):
+    
+    if run_config.gpu:
+        assert "gpu" in run_config.queue, \
+            "must submit GPU queue if GPU is requested"
+    
     pass
 
 
@@ -54,6 +71,14 @@ def parse_whitelist_config(run_cfg: RunConfig) -> str:
     return whitelist[:-4]
 
 
+def parse_queue_config(run_cfg: RunConfig) -> str:
+    
+    if run_cfg.gpu:
+        return GPUQueue(run_cfg.queue).value
+    else:
+        return CPUQueue(run_cfg.queue).value
+
+
 def parse_gpu_config(run_cfg: RunConfig) -> str:
 
     if run_cfg.gpu:
@@ -72,52 +97,62 @@ def main():
     # We remove 'config' attribute from config as the underlying DataClass does not have it
     del cli_cfg.config
 
-    default_cfg = OmegaConf.structured(ExperimentConfig)
-    cfg = OmegaConf.merge(default_cfg, file_cfg, cli_cfg)
-
+    default_cfg = OmegaConf.structured(RunConfig)
+    cfg = OmegaConf.merge(default_cfg, file_cfg, cli_cfg) # type: ignore
+    cfg: RunConfig = OmegaConf.to_object(cfg) # type: ignore
     submit_script = cfg.submit_script
 
-    for run_cfg in cfg.runs:
-        validate_run_config(run_cfg)
+    validate_run_config(cfg)
 
-        command = "bsub"
+    base_command = "bsub"
+    
+    base_command += f" -R "
+    base_command += "\""
+    
+    memory_args = parse_memory_config(cfg)
+    base_command += memory_args
         
-        command += f" -R "
-        command += "\""
-        
-        memory_args = parse_memory_config(run_cfg)
-        command += memory_args
-            
-        blacklist = parse_blacklist_config(run_cfg)
-        if blacklist != "":
-            command += f" select[{blacklist}]"
-        
-        whitelist = parse_whitelist_config(run_cfg)
-        if whitelist != "":
-            command += f" select[{whitelist}]"
-        
-        command += "\""
-        
-        if run_cfg.gpu:
-            gpu_args = parse_gpu_config(run_cfg)
-            command += f" -gpu {gpu_args}"
+    blacklist = parse_blacklist_config(cfg)
+    if blacklist != "":
+        base_command += f" select[{blacklist}]"
+    
+    whitelist = parse_whitelist_config(cfg)
+    if whitelist != "":
+        base_command += f" select[{whitelist}]"
+    
+    base_command += "\""
+    
+    if cfg.gpu:
+        gpu_args = parse_gpu_config(cfg)
+        base_command += f" -gpu {gpu_args}"
 
-        if run_cfg.stdout:
-            os.makedirs(os.path.dirname(run_cfg.stdout), exist_ok=True)
-            command += f" -o {run_cfg.stdout}"
+    if cfg.stdout:
+        os.makedirs(os.path.dirname(cfg.stdout), exist_ok=True)
+        base_command += f" -o {cfg.stdout}"
 
-        if run_cfg.stderr:
-            os.makedirs(os.path.dirname(run_cfg.stderr), exist_ok=True)
-            command += f" -e {run_cfg.stderr}"
+    if cfg.stderr:
+        os.makedirs(os.path.dirname(cfg.stderr), exist_ok=True)
+        base_command += f" -e {cfg.stderr}"
 
-        command += f" -q {run_cfg.queue}"
+    queue = parse_queue_config(cfg)
+    base_command += f" -q {queue}"
 
-        command += (
-            f' /bin/bash -l -c "{submit_script} {run_cfg.script} {run_cfg.script_args}"'
-        )
-        print(f"{run_cfg.name}")
-        print(f"{command}")
+    base_command += (
+        f' /bin/bash -l -c "{submit_script} {cfg.script} {cfg.script_args}'
+    )
+    print(f'{base_command}"')
 
+    if len(cfg.script_overrides) > 0:
+        for override in cfg.script_overrides:
+            command = base_command + f' {override}"'
+            print(f"{command}")
+            try:
+                subprocess.run(command, check=True, shell=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Script exited with error: {e.returncode}")
+                sys.exit(e.returncode)
+    else:
+        command = base_command + '"'
         try:
             subprocess.run(command, check=True, shell=True)
         except subprocess.CalledProcessError as e:
